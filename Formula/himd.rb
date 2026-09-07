e26ba14b63e6b26130997a78341b7432f98d4bd7
mo kasaei <45795173+mkasaii16@users.noreply.github.com>
2026-09-07 19:38:40 +0200

    Update himd to 0.0.4

--- Formula/himd.rb
+++ Formula/himd.rb
@@ -1,20 +1,20 @@
 class Himd < Formula
   desc "Analyze and search your shell command history"
   homepage "https://github.com/mkasaii16/history_cmd"
-  url "https://github.com/mkasaii16/history_cmd/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "d9bd266b142c52ad86c57c9326c38b12eae6c64fe26d7fdb504f5a69324aaecf"
+  url "https://github.com/mkasaii16/history_cmd/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "567a8ed9a558f930f2bb3da0f79bfa8159db66587893060a2262610312cc6106"
   license "MIT"
   depends_on :macos
 
   resource "binary" do
     on_arm do
-      url "https://github.com/mkasaii16/history_cmd/releases/download/v0.0.3/himd-aarch64-apple-darwin.tar.gz"
-      sha256 "079694e9687f8848d9b23551caf42a8064434ced61e5d756ef419e37081cdbda"
+      url "https://github.com/mkasaii16/history_cmd/releases/download/v0.0.4/himd-aarch64-apple-darwin.tar.gz"
+      sha256 "7e7788e1cad6d9a58ac62ce2fe3bbd48e043de84b25c2cb86f8565d3ed8782e5"
     end
 
     on_intel do
-      url "https://github.com/mkasaii16/history_cmd/releases/download/v0.0.3/himd-x86_64-apple-darwin.tar.gz"
-      sha256 "afccb6c9969c38c330c55411f58dc53a598d578aaea5fbcce7b1fc4f7a7ece93"
+      url "https://github.com/mkasaii16/history_cmd/releases/download/v0.0.4/himd-x86_64-apple-darwin.tar.gz"
+      sha256 "4e79bb404943f51fd8b350a097defe3ffccbb8d0526dfcbb9c2d1c2968fad73b"
     end
   end
 
